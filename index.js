95242f9d153bf1ac34bc156f849c8d06c6100860
Jeff Weimer <jweimer@weareenvoy.com>
2018-07-13 16:50:33 -0700

    cors

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ admin.initializeApp({
 app = express()
 app.use(compression())
 app.use(bodyParser.json())
-app.use(cors())
-app.options('*', cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 const port = process.env.PORT || 4000
 
