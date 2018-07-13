bf7d1ad7f3e88994911895507400cd11e5788a4f
Jeff Weimer <jweimer@weareenvoy.com>
2018-07-13 16:29:15 -0700

    env var fix

--- index.js
+++ index.js
@@ -7,16 +7,8 @@ const bodyParser = require('body-parser')
 const compression = require('compression')
 
 const serviceAccount = {
-  type: 'service_account',
-  project_id: 'envoy-carwash',
-  private_key_id: `${process.env.private_key_id}`,
-  private_key: `${process.env.private_key}`,
-  client_email: `${process.env.client_email}`,
-  client_id: `${process.env.client_id}`,
-  auth_uri: `${process.env.auth_uri}`,
-  token_uri: `${process.env.token_uri}`,
-  auth_provider_x509_cert_url: `${process.env.auth_provider_x509_cert_url}`,
-  client_x509_cert_url: `${process.env.client_x509_cert_url}`
+  private_key: process.env.private_key.replace(/\\n/g, '\n'),
+  client_email: `${process.env.client_email}`
 }
 
 admin.initializeApp({
