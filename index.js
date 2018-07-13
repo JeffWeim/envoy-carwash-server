ee2a56174c482ab4188e01d579c850836897ab33
Jeff Weimer <jweimer@weareenvoy.com>
2018-07-13 16:56:27 -0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const admin = require('firebase-admin')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const compression = require('compression')
 
 const serviceAccount = {
   private_key: process.env.private_key.replace(/\\n/g, '\n'),
@@ -17,13 +16,8 @@ admin.initializeApp({
 })
 
 app = express()
-app.use(compression())
+app.use(cors())
 app.use(bodyParser.json())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
 
 const port = process.env.PORT || 4000
 
